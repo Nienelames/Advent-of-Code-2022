ed75f59b8014fe0b6f6df85c0e56f9b3ad4d158f
Karolis Rauckis <94575330+Nienelames@users.noreply.github.com>
2022-12-05 23:19:54 +0000

    Add files via upload

---
+++ aoc-day1-csharp/Advent of Code - Day 1/CalorieCalculator.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine(CaloreiesCalculator.getMaxCalories(System.IO.File.ReadAllText(@"C:\Users\Ponas Amungas\Downloads\input.txt")));
+Console.WriteLine(CaloreiesCalculator.getTopThreeMaxCalories(System.IO.File.ReadAllText(@"C:\Users\Ponas Amungas\Downloads\input.txt")));
+
+public class CaloreiesCalculator
+{
+   public static int getMaxCalories(string caloriesData)
+    {
+        return caloriesData
+            .Split("\n\n")
+            .Select(row =>
+                row.Split("\n")
+                   .Select(calories => int.Parse(row))
+                   .Sum()
+            )
+            .Max();
+    }
+
+    public static int getTopThreeMaxCalories(string caloriesData)
+    {
+        return caloriesData
+            .Split("\n\n")
+            .Select(row =>
+                row.Split("\n")
+                   .Select(calories => int.Parse(calories))
+                   .Sum()
+            )
+            .OrderByDescending(i => i)
+            .Take(3)
+            .Sum();
+    }
+}
